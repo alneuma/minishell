bcd69823519ed73dd442f1c910f9ea988f2909ac
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2025-05-27 17:32:19 +0200

    chore: implemented t_stack_char to improve tokenizer; not yet tested

--- Makefile
+++ Makefile
@@ -17,9 +17,11 @@ vpath %.c $(SRC_DIR)/scanner
 SRC	+= scanner.c
 SRC	+= token.c
 SRC	+= token_types.c
-vpath %.c $(SRC_DIR)/data_structures
+vpath %.c $(SRC_DIR)/data_structures/queue
 SRC	+= queue.c
 SRC	+= queue_init.c
+vpath %.c $(SRC_DIR)/data_structures/stack_char
+SRC	+= stack_char.c
 vpath %.c $(SRC_DIR)/execute
 SRC	+= execute.c
 SRC	+= heredoc.c
--- inc/data_structures.h
+++ inc/data_structures.h
@@ -2,6 +2,7 @@
 # define DATA_STRUCTURES_H
 
 typedef struct s_queue	t_queue;
+typedef struct s_stack_char	t_stack_char;
 
 // queue
 int		queue_peek(void **data, t_queue *queue);
@@ -9,5 +10,13 @@ int		queue_dequeue(void **data, t_queue *queue);
 int		queue_enqueue(void *data, t_queue *queue);
 int		queue_init(t_queue **queue);
 void	queue_destroy(t_queue **queue);
+
+// stack char
+int		stack_char_size(t_stack_char *stack);
+int		stack_char_peek(char *c, t_stack_char *stack);
+int		stack_char_pop(char *c, t_stack_char *stack);
+int		stack_char_push(char c, t_stack_char *stack);
+int		stack_char_init(t_stack_char **stack);
+void	stack_char_destroy(t_stack_char **stack);
 
 #endif //DATA_STRUCTURES_H
