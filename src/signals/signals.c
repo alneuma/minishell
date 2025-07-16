5984feb4c78c10740a5ed16790708490974eb408
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2025-07-16 11:17:56 +0200

    refactor(signals): moved call to rl_replace_line() from signal handler to readline hook

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -1,5 +1,4 @@
 #include <signal.h>
-// #include <unistd.h>
 #include <readline/readline.h>
 
 static volatile sig_atomic_t	g_signum = 0;
@@ -9,23 +8,14 @@ int	readline_hook(void);
 void	handler_sigint(int signum)
 {
 	(void)signum;
-	// g_signum = SIGINT;
-	// ft_printf("%d\n", SIGINT);
-	// ft_printf("%d\n", g_signum);
-	// char	buf[1];
-	// buf[0] = EOF;
-	// rl_replace_line(buf, 1);
-	rl_replace_line("", 1);
 	rl_done = 1;
 	g_signum = SIGINT;
-	// ft_printf("after %d\n", SIGINT);
-	// ft_printf("after %d\n", g_signum);
-	// rl_on_new_line();
-	// printf("%d\n", signum);
 }
 
 int	readline_hook(void)
 {
+	if (rl_done == 1)
+		rl_replace_line("", 1);
 	return (0);
 }
 
