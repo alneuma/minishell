885d051508836415f93e5636c6eb1a10b033791d
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2025-07-17 12:12:44 +0200

    fix(echo): now correctly handles n option if provided multiple times

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -2,6 +2,8 @@
 #include "libft.h"
 #include "variables.h"
 
+static int	is_n_option(const char *str);
+
 int builtin_echo(const char **argv, int fd_in, int fd_out, t_env *env)
 {
 	int	i;
@@ -10,18 +12,32 @@ int builtin_echo(const char **argv, int fd_in, int fd_out, t_env *env)
 	(void)fd_in;
 	(void)env;
 	i = 1;
-	opt_n = 0;
-	if (argv[1] != NULL && ft_strlen(argv[1]) == 2 && argv[1][0] == '-'
-		&& argv[1][1] == 'n')
-		opt_n = 1;
-	i += opt_n;
+	while (argv[i] != NULL && is_n_option(argv[i]))
+		i++;
+	opt_n = (i != 1);
 	while (argv[i] != NULL)
 	{
-		if (i > 1)
-			ft_dprintf(fd_out, " ");
 		ft_dprintf(fd_out, "%s", argv[i++]);
+		if (argv[i] != NULL)
+			ft_dprintf(fd_out, " ");
 	}
 	if (!opt_n)
 		ft_dprintf(fd_out, "\n");
 	return (0);
+}
+
+static int	is_n_option(const char *str)
+{
+	int	i;
+
+	if (str == NULL || ft_strlen(str) < 2 || str[0] != '-' || str[1] != 'n')
+		return (0);
+	i = 2;
+	while (str[i] != '\0')
+	{
+		if (str[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
 }
