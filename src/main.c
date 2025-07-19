d8dec3cec4b4e01390992ca2134436f0b58c32ad
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2025-07-19 18:30:29 +0200

    fix(exit): now returns the correct code when calling exit with argument

--- src/main.c
+++ src/main.c
@@ -132,7 +132,7 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		return_code = shell_iteration(&env);
-		if (return_code == 0)
+		if (return_code == 0 && env.exit == 0)
 			env.code = 0;
 		if (is_fatal(return_code) || env.exit)
 		{
