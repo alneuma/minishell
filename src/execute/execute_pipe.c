9961de2cebee6841a4cb25c99217bf49c6f24a21
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2025-07-23 21:48:42 +0200

    refactor(execute_pipe.c): split up functions to make them norm conforming

--- src/execute/execute_pipe.c
+++ src/execute/execute_pipe.c
@@ -10,7 +10,8 @@
 #include "execute.h"
 #include "signals.h"
 
-int	execute_child(pid_t *pid, t_token *tree, int fds[2], t_env *env);
+int	pipe_fork_child(pid_t *pid, t_token *tree, int fds[2], t_env *env);
+int	execute_child(int fds[4], t_token *tree, t_env *env);
 
 int	pipe_setup_fds(int fds_1[4], int fds_2[4], int fd_in, int fd_out)
 {
@@ -34,22 +35,25 @@ int	pipe_setup_fds(int fds_1[4], int fds_2[4], int fd_in, int fd_out)
 	return (0);
 }
 
-int	execute_pipe(t_token *tree, int fd_in, int fd_out, t_env *env)
+int	wait_child(int *wstatus, pid_t pid, int fds[2])
 {
-	int		return_code;
-	int		fds_1[4];
-	int		fds_2[4];
-	pid_t	pid_1;
-	pid_t	pid_2;
+	int	return_code;
+	int	rc_fds;
 
-	return_code = pipe_setup_fds(fds_1, fds_2, fd_in, fd_out);
+	if (waitpid(pid, &wstatus, 0) < 0)
+	{
+		return_code = errno;
+		errno = 0;
+	}
+	rc_fds = close_fd_safe(fds[0]);
+	rc_fds |= close_fd_safe(fds[1]);
 	if (return_code)
 		return (return_code);
-	return_code = execute_child(&pid_1, tree->left, fds_1, env);
-	return_code = execute_child(&pid_2, tree->right, fds_2, env);
-	waitpid(pid_1, &return_code, 0);
-	close_fd_safe(fds_1[0]);
-	close_fd_safe(fds_1[1]);
+	return (rc_fds);
+}
+
+void	process_wstatus_left(int wstatus, t_env *env)
+{
 	if (env->pipe_lvl != 0)
 		env->code = WEXITSTATUS(return_code);
 	if (env->pipe_lvl == 0)
@@ -59,9 +63,10 @@ int	execute_pipe(t_token *tree, int fd_in, int fd_out, t_env *env)
 		if (WEXITSTATUS(return_code) == 130)
 			signum_set(SIGINT);
 	}
-	waitpid(pid_2, &return_code, 0);
-	close_fd_safe(fds_2[0]);
-	close_fd_safe(fds_2[1]);
+}
+
+int	process_wstatus_right(int wstatus, t_env *env)
+{
 	if (env->pipe_lvl == 0)
 		env->code = WEXITSTATUS(return_code);
 	if (env->code == 131)
@@ -73,7 +78,41 @@ int	execute_pipe(t_token *tree, int fd_in, int fd_out, t_env *env)
 	return (0);
 }
 
-int	execute_child(pid_t *pid, t_token *tree, int fds[4], t_env *env)
+// typedef struct s_child_info
+// {
+// 	pid_t	pid;
+// 	int		fd_in;
+// 	int		fd_out;
+// 	int		fd_garbage_1;
+// 	int		fd_garbage_2;
+// }	t_pipe_info;
+// 	t_pipe_info	pinfo_1;
+// 	t_pipe_info	pinfo_2;
+
+int	execute_pipe(t_token *tree, int fd_in, int fd_out, t_env *env)
+{
+	int		return_code;
+	int		fds_1[4];
+	int		fds_2[4];
+	pid_t	pid_1;
+	pid_t	pid_2;
+
+	return_code = pipe_setup_fds(fds_1, fds_2, fd_in, fd_out);
+	if (return_code)
+		return (return_code);
+	return_code = pipe_fork_child(&pid_1, tree->left, fds_1, env);
+	if (return_code)
+		return (return_code);
+	return_code = pipe_fork_child(&pid_2, tree->right, fds_2, env);
+	if (return_code)
+		return (return_code);
+	wait_child(&return_code, pid_1, fds_1);
+	process_wstatus_left(return_code, env);
+	wait_child(&return_code, pid_2, fds_2);
+	return (process_wstatus_right(return_code, env);
+}
+
+int	pipe_fork_child(pid_t *pid, t_token *tree, int fds[4], t_env *env)
 {
 	int		return_code;
 	int		env_code;
@@ -90,27 +129,33 @@ int	execute_child(pid_t *pid, t_token *tree, int fds[4], t_env *env)
 		return (return_code);
 	}
 	else if (*pid == 0)
+		exit(execute_child(fds, tree, env));
+	return (0);
+}
+
+int	execute_child(int fds[4], t_token *tree, t_env *env)
+{
+	int	return_code;
+	int	rc_fds;
+
+	env->pipe_lvl++;
+	rc_fds = close_fd_safe(fds[3]);
+	rc_fds |= close_fd_safe(fds[2]);
+	if (rc_fds)
 	{
-		env->pipe_lvl++;
-		return_code = close_fd_safe(fds[3]);
-		return_code = close_fd_safe(fds[2]);
-		if (return_code)
-		{
-			env_clear(env);
-			close_fd_safe(fds[1]);
-			close_fd_safe(fds[0]);
-			exit(return_code);
-		}
-		return_code = execute(tree, fds[0], fds[1], env);
-		env_code = env->code;
 		env_clear(env);
 		close_fd_safe(fds[1]);
 		close_fd_safe(fds[0]);
-		if (return_code > 0)
-			exit(return_code);
-		exit(env_code);
+		return (return_code);
 	}
-	else if (*pid > 0)
-		;
-	return (0);
+	return_code = execute(tree, fds[0], fds[1], env);
+	env_code = env->code;
+	env_clear(env);
+	rc_fds = close_fd_safe(fds[1]);
+	rc_fds |= close_fd_safe(fds[0]);
+	if (return_code > 0)
+		return (return_code);
+	if (rc_fds)
+		return (rc_fds);
+	return (env_code);
 }
