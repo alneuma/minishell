99408e5b8a18b5ac24d359d915b5fa112b8465d5
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2025-07-23 22:16:14 +0200

    refactor, bug(piping): introduced t_child_info struct; cat | ls does not work as required

--- src/execute/execute_pipe.c
+++ src/execute/execute_pipe.c
@@ -10,10 +10,19 @@
 #include "execute.h"
 #include "signals.h"
 
-int	pipe_fork_child(pid_t *pid, t_token *tree, int fds[2], t_env *env);
-int	execute_child(int fds[4], t_token *tree, t_env *env);
+typedef struct s_child_info
+{
+	pid_t	pid;
+	int		fd_in;
+	int		fd_out;
+	int		fd_garbage[2];
+}	t_child_info;
 
-int	pipe_setup_fds(int fds_1[4], int fds_2[4], int fd_in, int fd_out)
+int	pipe_fork_child(t_child_info *chinfo, t_token *tree, t_env *env);
+int	execute_child(t_child_info *chinfo, t_token *tree, t_env *env);
+
+int	pipe_setup_fds(t_child_info *chinfo_1, t_child_info *chinfo_2, int fd_in,
+		int fd_out)
 {
 	int	fds[2];
 	int	return_code;
@@ -24,29 +33,30 @@ int	pipe_setup_fds(int fds_1[4], int fds_2[4], int fd_in, int fd_out)
 		errno = 0;
 		return (return_code);
 	}
-	fds_1[0] = fd_in;
-	fds_1[1] = fds[1];
-	fds_1[2] = fds[0];
-	fds_1[3] = fd_out;
-	fds_2[0] = fds[0];
-	fds_2[1] = fd_out;
-	fds_2[2] = fds[1];
-	fds_2[3] = fd_in;
+	chinfo_1->fd_in = fd_in;
+	chinfo_1->fd_out = fds[1];
+	chinfo_1->fd_garbage[0] = fds[0];
+	chinfo_1->fd_garbage[1] = fd_out;
+	chinfo_2->fd_in = fds[0];
+	chinfo_2->fd_out = fd_out;
+	chinfo_2->fd_garbage[0] = fds[1];
+	chinfo_2->fd_garbage[1] = fd_in;
 	return (0);
 }
 
-int	wait_child(int *wstatus, pid_t pid, int fds[2])
+int	wait_child(int *wstatus, t_child_info *chinfo)
 {
 	int	return_code;
 	int	rc_fds;
 
-	if (waitpid(pid, &wstatus, 0) < 0)
+	return_code = 0;
+	if (waitpid(chinfo->pid, wstatus, 0) < 0)
 	{
 		return_code = errno;
 		errno = 0;
 	}
-	rc_fds = close_fd_safe(fds[0]);
-	rc_fds |= close_fd_safe(fds[1]);
+	rc_fds = close_fd_safe(chinfo->fd_in);
+	rc_fds |= close_fd_safe(chinfo->fd_out);
 	if (return_code)
 		return (return_code);
 	return (rc_fds);
@@ -55,12 +65,12 @@ int	wait_child(int *wstatus, pid_t pid, int fds[2])
 void	process_wstatus_left(int wstatus, t_env *env)
 {
 	if (env->pipe_lvl != 0)
-		env->code = WEXITSTATUS(return_code);
+		env->code = WEXITSTATUS(wstatus);
 	if (env->pipe_lvl == 0)
 	{
-		if (WEXITSTATUS(return_code) == 131)
+		if (WEXITSTATUS(wstatus) == 131)
 			signum_set(SIGQUIT);
-		if (WEXITSTATUS(return_code) == 130)
+		if (WEXITSTATUS(wstatus) == 130)
 			signum_set(SIGINT);
 	}
 }
@@ -68,7 +78,7 @@ void	process_wstatus_left(int wstatus, t_env *env)
 int	process_wstatus_right(int wstatus, t_env *env)
 {
 	if (env->pipe_lvl == 0)
-		env->code = WEXITSTATUS(return_code);
+		env->code = WEXITSTATUS(wstatus);
 	if (env->code == 131)
 		signum_set(SIGQUIT);
 	if (env->code == 130)
@@ -78,81 +88,68 @@ int	process_wstatus_right(int wstatus, t_env *env)
 	return (0);
 }
 
-// typedef struct s_child_info
-// {
-// 	pid_t	pid;
-// 	int		fd_in;
-// 	int		fd_out;
-// 	int		fd_garbage_1;
-// 	int		fd_garbage_2;
-// }	t_pipe_info;
-// 	t_pipe_info	pinfo_1;
-// 	t_pipe_info	pinfo_2;
-
 int	execute_pipe(t_token *tree, int fd_in, int fd_out, t_env *env)
 {
-	int		return_code;
-	int		fds_1[4];
-	int		fds_2[4];
-	pid_t	pid_1;
-	pid_t	pid_2;
+	t_child_info	chinfo_left;
+	t_child_info	chinfo_right;
+	int				return_code;
 
-	return_code = pipe_setup_fds(fds_1, fds_2, fd_in, fd_out);
+	return_code = pipe_setup_fds(&chinfo_left, &chinfo_right, fd_in, fd_out);
 	if (return_code)
 		return (return_code);
-	return_code = pipe_fork_child(&pid_1, tree->left, fds_1, env);
+	return_code = pipe_fork_child(&chinfo_left, tree->left, env);
 	if (return_code)
 		return (return_code);
-	return_code = pipe_fork_child(&pid_2, tree->right, fds_2, env);
+	return_code = pipe_fork_child(&chinfo_right, tree->right, env);
 	if (return_code)
 		return (return_code);
-	wait_child(&return_code, pid_1, fds_1);
+	wait_child(&return_code, &chinfo_left);
 	process_wstatus_left(return_code, env);
-	wait_child(&return_code, pid_2, fds_2);
-	return (process_wstatus_right(return_code, env);
+	wait_child(&return_code, &chinfo_right);
+	return (process_wstatus_right(return_code, env));
 }
 
-int	pipe_fork_child(pid_t *pid, t_token *tree, int fds[4], t_env *env)
+int	pipe_fork_child(t_child_info *chinfo, t_token *tree, t_env *env)
 {
 	int		return_code;
-	int		env_code;
 
-	*pid = fork();
-	if (*pid < 0)
+	chinfo->pid = fork();
+	if (chinfo->pid < 0)
 	{
 		return_code = errno;
 		errno = 0;
-		close_fd_safe(fds[3]);
-		close_fd_safe(fds[2]);
-		close_fd_safe(fds[1]);
-		close_fd_safe(fds[0]);
+		close_fd_safe(chinfo->fd_in);
+		close_fd_safe(chinfo->fd_out);
+		close_fd_safe(chinfo->fd_garbage[0]);
+		close_fd_safe(chinfo->fd_garbage[1]);
 		return (return_code);
 	}
-	else if (*pid == 0)
-		exit(execute_child(fds, tree, env));
+	else if (chinfo->pid == 0)
+		exit(execute_child(chinfo, tree, env));
 	return (0);
 }
 
-int	execute_child(int fds[4], t_token *tree, t_env *env)
+int	execute_child(t_child_info *chinfo, t_token *tree, t_env *env)
 {
 	int	return_code;
 	int	rc_fds;
+	int	env_code;
 
 	env->pipe_lvl++;
-	rc_fds = close_fd_safe(fds[3]);
-	rc_fds |= close_fd_safe(fds[2]);
+	rc_fds = close_fd_safe(chinfo->fd_garbage[0]);
+	rc_fds |= close_fd_safe(chinfo->fd_garbage[1]);
 	if (rc_fds)
 	{
 		env_clear(env);
-		close_fd_safe(fds[1]);
-		close_fd_safe(fds[0]);
-		return (return_code);
+		close_fd_safe(chinfo->fd_in);
+		close_fd_safe(chinfo->fd_out);
+		return (rc_fds);
 	}
-	return_code = execute(tree, fds[0], fds[1], env);
+	return_code = execute(tree, chinfo->fd_in, chinfo->fd_out, env);
 	env_code = env->code;
 	env_clear(env);
-	rc_fds = close_fd_safe(fds[1]);
-	rc_fds |= close_fd_safe(fds[0]);
+	rc_fds = close_fd_safe(chinfo->fd_in);
+	rc_fds |= close_fd_safe(chinfo->fd_out);
 	if (return_code > 0)
 		return (return_code);
 	if (rc_fds)
