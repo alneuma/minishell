076115396aeaba0f696459ced90ec97c46b0c3d8
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2025-08-03 20:29:00 +0200

    fix(cd): fixed bug which made it possible for cd to change into deleted parent directory

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -9,79 +9,10 @@
 #include "defs.h"
 #include "builtins_internals.h"
 
-int	update_env(const char *old_cwd, t_env *env);
-int	get_objective_dir(char **objective, const char **argv, t_env *env);
-int	ft_chdir(const char *objective);
-
-int	get_objective_dir(char **objective, const char **argv, t_env *env)
-{
-	char	*tmp;
-
-	*objective = NULL;
-	tmp = (char *)argv[1];
-	if (string_array_get_len(argv) > 2)
-	{
-		print_error_str(" cd:", STR_TOO_MANY_ARGUMENTS);
-		env->code = ERR_CD;
-		return (-1);
-	}
-	if (string_array_get_len(argv) < 2)
-	{
-		tmp = variable_set_var_get_ref(env->vars, "HOME");
-		if (tmp == NULL)
-		{
-			print_error_str(" cd:", STR_HOME_NOT_SET);
-			env->code = ERR_CD;
-			return (-1);
-		}
-	}
-	*objective = ft_strdup(tmp);
-	if (*objective == NULL)
-		return (ENOMEM);
-	return (0);
-}
-
-int	ft_chdir(const char *objective)
-{
-	int	return_code;
-
-	if (chdir(objective) < 0)
-	{
-		return_code = errno;
-		errno = 0;
-		if (!is_fatal(return_code))
-			print_error("cd", return_code);
-		return (return_code);
-	}
-	return (0);
-}
-
-int	update_env(const char *old_cwd, t_env *env)
-{
-	int		return_code;
-
-	free(env->cwd);
-	env->cwd = NULL;
-	return_code = ft_get_cwd(&env->cwd);
-	if (is_fatal(return_code))
-		return (return_code);
-	if (return_code)
-	{
-		print_error("cd", return_code);
-		env->code = ERR_CD;
-		return (-1);
-	}
-	return_code = 0;
-	if (variable_set_var_get_ref(env->vars, "OLDPWD") != NULL)
-	{
-		return_code = variable_set_var_set(env->vars, "OLDPWD", old_cwd, 0);
-		if (return_code)
-			return (return_code);
-	}
-	if (variable_set_var_get_ref(env->vars, "PWD") != NULL)
-		return_code = variable_set_var_set(env->vars, "PWD", env->cwd, 0);
-	return (return_code);
-}
+static int	update_env(const char *old_cwd, t_env *env);
+static int	get_objective_dir(char **objective, const char **argv, t_env *env);
+static int	ft_chdir(const char *objective);
+static int	handle_relative(char **objective, t_env *env);
 
 int	builtin_cd(const char **argv, int fd_in, int fd_out, t_env *env)
 {
@@ -109,5 +40,101 @@ int	builtin_cd(const char **argv, int fd_in, int fd_out, t_env *env)
 	}
 	return_code = update_env(old_cwd, env);
 	free(old_cwd);
+	return (return_code);
+}
+
+static int	get_objective_dir(char **objective, const char **argv, t_env *env)
+{
+	char	*tmp;
+
+	*objective = NULL;
+	tmp = (char *)argv[1];
+	if (string_array_get_len(argv) > 2)
+	{
+		print_error_str(" cd:", STR_TOO_MANY_ARGUMENTS);
+		env->code = ERR_CD;
+		return (-1);
+	}
+	if (string_array_get_len(argv) < 2)
+	{
+		tmp = variable_set_var_get_ref(env->vars, "HOME");
+		if (tmp == NULL)
+		{
+			print_error_str(" cd:", STR_HOME_NOT_SET);
+			env->code = ERR_CD;
+			return (-1);
+		}
+	}
+	*objective = ft_strdup(tmp);
+	if (*objective == NULL)
+		return (ENOMEM);
+	return (handle_relative(objective, env));
+}
+
+static int	handle_relative(char **objective, t_env *env)
+{
+	char	*tmp1;
+	char	*tmp2;
+
+	if (**objective != '.')
+		return (0);
+	tmp1 = ft_strjoin(env->cwd, "/");
+	if (tmp1 == NULL)
+	{
+		free(*objective);
+		*objective = NULL;
+		return (ENOMEM);
+	}
+	tmp2 = ft_strjoin(tmp1, *objective);
+	free(tmp1);
+	free(*objective);
+	if (tmp2 == NULL)
+	{
+		*objective = NULL;
+		return (ENOMEM);
+	}
+	*objective = tmp2;
+	return (0);
+}
+
+static int	ft_chdir(const char *objective)
+{
+	int	return_code;
+
+	if (chdir(objective) < 0)
+	{
+		return_code = errno;
+		errno = 0;
+		if (!is_fatal(return_code))
+			print_error("cd", return_code);
+		return (return_code);
+	}
+	return (0);
+}
+
+static int	update_env(const char *old_cwd, t_env *env)
+{
+	int		return_code;
+
+	free(env->cwd);
+	env->cwd = NULL;
+	return_code = ft_get_cwd(&env->cwd);
+	if (is_fatal(return_code))
+		return (return_code);
+	if (return_code)
+	{
+		print_error("cd", return_code);
+		env->code = ERR_CD;
+		return (-1);
+	}
+	return_code = 0;
+	if (variable_set_var_get_ref(env->vars, "OLDPWD") != NULL)
+	{
+		return_code = variable_set_var_set(env->vars, "OLDPWD", old_cwd, 0);
+		if (return_code)
+			return (return_code);
+	}
+	if (variable_set_var_get_ref(env->vars, "PWD") != NULL)
+		return_code = variable_set_var_set(env->vars, "PWD", env->cwd, 0);
 	return (return_code);
 }
